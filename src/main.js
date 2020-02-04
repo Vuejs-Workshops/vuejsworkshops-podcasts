6a8e10d5574dd474875cf69b045a372ad7c8d936
dragosh <stefanescu.dragos@gmail.com>
2020-02-04 22:12:04 +0200

    graphql implementation

--- src/main.js
+++ src/main.js
@@ -12,6 +12,25 @@ Vue.config.productionTip = false;
 Vue.filter('formatDate', function (dt) {
   if (dt && dt.indexOf("T") >= 0) return dt.split("T")[0]
 })
+
+/* eslint-disable */
+//GraphQL
+import ApolloClient from 'apollo-boost'
+
+const apolloClient = new ApolloClient({
+    uri: 'https://jsnoise.herokuapp.com/graphql'
+});
+
+import VueApollo from 'vue-apollo'
+import gql from "graphql-tag";
+
+Vue.use(VueApollo)
+
+//The provider holds the Apollo client instances that can then be used by all the child components.
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+})
+
 import Vuex from 'vuex'
 Vue.use(Vuex)
 
@@ -56,7 +75,7 @@ var store = new Vuex.Store({
       dispatch("loadPodcasts");
     },
 
-    loadPodcast({ commit }, showId) {
+    loadPodcastREST({ commit }, showId) {
       axios
         .get(`https://jsnoise.herokuapp.com/api/shows/${showId}`)
         .then(response => {
@@ -64,12 +83,61 @@ var store = new Vuex.Store({
           commit("LOAD_PODCAST", response.data);
         })
         .catch(err => console.dir(err));
-    }
+    },
+    loadPodcasts({ commit, state }) {
+      apolloClient
+          .query({
+          query: gql`
+              query shows($page: Int) {
+              showsList(page: $page) {
+                  first
+                  last
+                  shows {
+                  id
+                  title
+                  mp3
+                  publishedDate
+                  producerName
+                  producerId
+                  }
+              }
+              }
+          `,
+          variables: { page: state.page }
+          })
+          .then(response => {
+          commit("LOAD_PODCASTS", response.data.showsList);
+          })
+          .catch(err => console.dir("gql err: ", err));
+  },
+  loadPodcast({ commit, state }, showId) {
+    apolloClient
+        .query({
+        query: gql`
+            query oneShow($id: Int!) {
+            show(id: $id) {
+                id
+                title
+                mp3
+                description
+                producerName
+                publishedDate
+            }
+            }
+        `,
+        variables: { id: showId }
+        })
+        .then(response => {
+        commit("LOAD_PODCAST", response.data.show);
+        })
+        .catch(err => console.dir("gql err: ", err));
+}
   }
 });
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
